5d908b4f9fc2fb61c2f27f848d3dd93144d3b6a8
LuciaPrebisch <luciaprebisch@gmail.com>
2023-02-24 09:38:39 -0300

    nuevos ejercicios

--- arraysejercicios.js
+++ arraysejercicios.js
@@ -53,4 +53,27 @@ Para eso, debemos crear una variable llamada alumnoDeBaja y, utilizando un méto
 let estudiantes = ["Lucia", "Jose" , "Simon" , "Amalia"]
 let alumnoDeBaja = estudiantes.shift()
 console.log (alumnoDeBaja)
+
+-----EJERCICIOS PARTE 2 ----
+
+1. Crear la estructura adecuada para guardar las siguientes películas:
+"star wars", "totoro", "rocky", "pulp fiction", "la vida es bella"
+Importante: verificá que todo funciona correctamente accediendo a alguna de
+las películas una vez creada la estructura correspondiente.
+2. Más tarde, de producción dieron el aviso de que las películas deberían estar
+todas en mayúsculas. Para esto solicitan que crees una función que reciba por
+parámetro un array y convierta el contenido de cada elemento a mayúsculas.
+Pista: revisá qué hace el método de strings .toUpperCase().
+
+
+
+let películas = ["nemo", "el rey leon", "intensamente", "cars"]
+console.log (películas[0])
+
+let peliFormateada = películas[1].toUpperCase("") // recordar poner siempre dsp de la varibable QUE QUIERO poner en mayuscula
+console.log(peliFormateada)
+
+let peliculasNuevas = películas.push("toy story", "kung fu panda", "red")
+console.log(peliculasNuevas)
+
 */
