0618f85d23eab08232f946a3b7126e6765a796ac
LuciaPrebisch <luciaprebisch@gmail.com>
2023-03-13 08:59:10 -0300

    JS

---
+++ ciclos.js
@@ -0,0 +1,13 @@
+/* los ciclos nos permiten repetir instrucciones de manera sencilla, una determinada cantidad de veces o bien hasta que se cumpla una condicion
+
+ejemplo: contar del 1 al 5 
+
+    INICIO      CONDICION    MODIFICADOR
+for (i= vuelta  , i<=5      ; vuelta ++) {
+    console.log("dando la vuelta numero" + i)
+}
+
+antes de ejecutar el codigo se pregunta si la condicion es verdadera (continua) si es falsa (detiene ciclo)
+
+*/
+
